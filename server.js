713427bcd6f4d7b72dc08f197064ec4a6f9aef90
victor-aguilar <74022888+vidi-server@users.noreply.github.com>
2022-04-25 10:18:16 -0500

    80

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
 
 const { port } = require('./config.json');
+const PORT = port || 80
 const proxy = require('cors-anywhere');
 proxy.createServer({
     originWhitelist: [],
     requireHeader: [],
     removeHeaders: ['user-agent']
-}).listen( port ||  process.env.PORT, function() {
-    console.log('Running CORS Anywhere on ::' + (port || process.env.PORT) + '::');
+}).listen( PORT, function() {
+    console.log('Running CORS Anywhere on ::' + PORT + '::');
 });
