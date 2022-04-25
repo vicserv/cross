b8c81ce48e2cef95006465f175d1a079259951f8
victor-aguilar <74022888+vidi-server@users.noreply.github.com>
2022-04-25 10:45:06 -0500

    4000

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const { port } = require('./config.json');
-const PORT =  process.env.PORT || 443;
+const PORT =  process.env.PORT || 3000;
 const proxy = require('cors-anywhere');
 proxy.createServer({
     originWhitelist: [],
